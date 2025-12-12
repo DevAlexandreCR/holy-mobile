d29480ecf9ea9255d235bd43560ecc673757e032
DevAlexandreCR <devalexandrecr@gmail.com>
2025-12-11 20:05:31 -0500

    feat: update app namespace and identifiers to gorda.holyverso, add new app icon assets

--- android/app/src/main/kotlin/com/example/holy_mobile/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.holy_mobile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/gorda/holyverso/MainActivity.kt
@@ -0,0 +1,5 @@
+package gorda.holyverso
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
